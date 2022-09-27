fca2aef35708e55968045be9f40eccbbf96a9e1e
WH1TE-SHAD0W <marek@culak.sk>
2022-09-27 19:17:27 +0200

    add colors

--- mines/Minesweaper.py
+++ mines/Minesweaper.py
@@ -6,11 +6,11 @@ import tkinter
 root = tkinter.Tk()
 
 # create canvas
-canvas = tkinter.Canvas(root, bg="white", height=850, width=850)
+canvas = tkinter.Canvas(root, bg="cyan", height=850, width=850)
 
-for i in range(11):
-    canvas.create_line(10, 10 + i * 80, 800, 10 + i * 80)
-    canvas.create_line(10 + i * 80, 10, 10 + i * 80, 800)
+# for i in range(11):
+    # canvas.create_line(10, 10 + i * 80, 800, 10 + i * 80)
+    # canvas.create_line(10 + i * 80, 10, 10 + i * 80, 800)
 # add to window and show
 canvas.pack()
 
@@ -35,18 +35,18 @@ importminearray()
 
 
 def generatemine(z, y):
-    for i in range(10):
+    i = 0
+    while i < 65:
         a = random.randint(1, 10)
         b = random.randint(1, 10)
-        print(z,a,y,b)
-        if a+2 >= z >= a-1 and b + 2 >= y >= b - 1:
+        print(z, a, y, b)
+        if a + 2 >= z >= a - 1 and b + 2 >= y >= b - 1:
             pass
         else:
             if x[a][b] != 'x':
                 x[a][b] = 'x'
-                print(',')
-            else:
-                i -= 1
+                print(',', i)
+                i += 1
 
 
 def rate(z, y):
@@ -78,7 +78,9 @@ def flood(z, y):
         for o in range(z - 1, z + 2):
             for u in range(y - 1, y + 2):
                 if x[o][u] != 9:
-                    canvas.create_text(80 * o - 40, 80 * u + -40, text=x[o][u])
+                    canvas.create_rectangle(o * 80, u * 80, o * 80 - 80, u * 80 - 80, fill='white', outline='')
+                    if x[o][u] != 0:
+                        canvas.create_text(80 * o - 40, 80 * u + -40, text=x[o][u])
         if x[z][y] == 0:
             x[z][y] = 9
             for o in range(z - 1, z + 2):
@@ -86,6 +88,7 @@ def flood(z, y):
                     if x[o][u] == 0:
                         flood(o, u)
     elif x[z][y] != 9:
+        canvas.create_rectangle(z*80,y*80,z*80-80,y*80-80, fill='white', outline='')
         canvas.create_text(80 * z - 40, 80 * y + -40, text=x[z][y])
 
 
@@ -109,6 +112,13 @@ def klik(a):
     n += 1
 
 
-canvas.bind("<Button-1>", klik)
+def rightclick(b):
+    x = b.x // 80
+    y = b.y // 80
+    canvas.create_polygon(x * 80 + 20, y * 80 + 40, x * 80 + 50, y * 80 + 60, x * 80 + 50, y * 80 + 20, outline='black')
+    canvas.create_line( x * 80 + 46, y * 80 + 50, x * 80 + 46 , y * 80 +80, width='10')
 
+
+canvas.bind("<Button-1>", klik)
+canvas.bind("<Button-3>", rightclick)
 root.mainloop()
