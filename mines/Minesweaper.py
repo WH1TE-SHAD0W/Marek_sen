1d047e31126cce47e2ab3b029f6b08d626edaaf2
Marek <marek@culak.sk>
2022-10-03 09:34:58 +0200

    miny s vyhrou

--- mines/Minesweaper.py
+++ mines/Minesweaper.py
@@ -1,3 +1,7 @@
+import os
+import subprocess
+import sys
+
 import generate_canvas
 import random
 import tkinter
@@ -9,8 +13,8 @@ root = tkinter.Tk()
 canvas = tkinter.Canvas(root, bg="cyan", height=850, width=850)
 
 # for i in range(11):
-    # canvas.create_line(10, 10 + i * 80, 800, 10 + i * 80)
-    # canvas.create_line(10 + i * 80, 10, 10 + i * 80, 800)
+# canvas.create_line(10, 10 + i * 80, 800, 10 + i * 80)
+# canvas.create_line(10 + i * 80, 10, 10 + i * 80, 800)
 # add to window and show
 canvas.pack()
 
@@ -31,21 +35,20 @@ def importminearray():
     x.append([9] * 12)
 
 
+
 importminearray()
 
 
 def generatemine(z, y):
     i = 0
-    while i < 65:
+    while i < 2:
         a = random.randint(1, 10)
         b = random.randint(1, 10)
-        print(z, a, y, b)
         if a + 2 >= z >= a - 1 and b + 2 >= y >= b - 1:
             pass
         else:
             if x[a][b] != 'x':
                 x[a][b] = 'x'
-                print(',', i)
                 i += 1
 
 
@@ -64,13 +67,11 @@ def ratewhole():
 
 
 def show():
+    print()
     for j in range(1, 11):
         for i in range(1, 11):
             print(x[j][i], end=' ')
         print()
-
-
-show()
 
 
 def flood(z, y):
@@ -81,6 +82,7 @@ def flood(z, y):
                     canvas.create_rectangle(o * 80, u * 80, o * 80 - 80, u * 80 - 80, fill='white', outline='')
                     if x[o][u] != 0:
                         canvas.create_text(80 * o - 40, 80 * u + -40, text=x[o][u])
+                        x[o][u] = 9
         if x[z][y] == 0:
             x[z][y] = 9
             for o in range(z - 1, z + 2):
@@ -88,37 +90,56 @@ def flood(z, y):
                     if x[o][u] == 0:
                         flood(o, u)
     elif x[z][y] != 9:
-        canvas.create_rectangle(z*80,y*80,z*80-80,y*80-80, fill='white', outline='')
+        canvas.create_rectangle(z * 80, y * 80, z * 80 - 80, y * 80 - 80, fill='white', outline='')
         canvas.create_text(80 * z - 40, 80 * y + -40, text=x[z][y])
 
 
+k = 0
+
+m = 0
 def towin(z, y):
+    global k,m
     if x[z][y] == 'x':
         canvas.create_text(400, 400, text='You loose')
-
-
+        k += 1
+    for i in range(12):
+        m += x[i].count(9)
+    if
 n = 0
 
 
 def klik(a):
-    x = a.x // 80 + 1
-    y = a.y // 80 + 1
-    global n
-    if n == 0:
-        generatemine(x, y)
-        ratewhole()
-    flood(x, y)
-    towin(x, y)
-    n += 1
+    global k,canvas,x
+    if k <= 0:
+        r = a.x // 80 + 1
+        q = a.y // 80 + 1
+        global n
+        if n == 0:
+            generatemine(r, q)
+            ratewhole()
+        show()
+        flood(r, q)
+        towin(r, q)
+        n += 1
 
 
-def rightclick(b):
-    x = b.x // 80
-    y = b.y // 80
+    else:
+        subprocess.call([sys.executable, os.path.realpath(__file__)] +
+        sys.argv[1:])
+        k -=1
+        x = []
+        importminearray()
+
+def flag(x,y):
     canvas.create_polygon(x * 80 + 20, y * 80 + 40, x * 80 + 50, y * 80 + 60, x * 80 + 50, y * 80 + 20, outline='black')
-    canvas.create_line( x * 80 + 46, y * 80 + 50, x * 80 + 46 , y * 80 +80, width='10')
-
-
+    canvas.create_line(x * 80 + 46, y * 80 + 50, x * 80 + 46, y * 80 + 80, width='10')
+def rightclick(b):
+    global x
+    if k <= 0:
+        r = b.x // 80 +1
+        q = b.y // 80+1
+        if x[r][q] == 'x' and x[r][q] != 9:
+            flag(r-1,q-1)
 canvas.bind("<Button-1>", klik)
 canvas.bind("<Button-3>", rightclick)
 root.mainloop()
